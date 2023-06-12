4734d37d0a0001fcea15fb679b28197aaf19e775
nH1217 <nharris1217@gmail.com>
2023-06-12 13:39:28 -0400

    Updated Styling

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
-console.log('hi')
+console.log('hi');
 
 // variable for the toggle button
-const menuButton = document.querySelector('.menu-toggle')
+const menuButton = document.querySelector('.menu-toggle');
 
 // variable for the nav element
-const nav = document.querySelector('nav')
+const nav = document.querySelector('nav');
 
-console.log(menuButton)
-console.log(nav)
+console.log(menuButton);
+console.log(nav);
 
 // funtion, which listens for the user to click the menu button
 // when the user clicks the button, add the open class to the nav element
 
 menuButton.addEventListener('click', () => {
-  nav.classList.toggle('open')
-})
+  nav.classList.toggle('open');
+});
 
 // Fetch Github
 
@@ -22,32 +22,46 @@ fetch('https://api.github.com/users/lily-fitz/repos')
   .then((response) => response.json())
   .then((data) => {
     // sort by date
-    data.sort((a, b) => new Date(b.created_at) - new Date(a.created_at))
+    data.sort((a, b) => new Date(b.created_at) - new Date(a.created_at));
 
     // limit the number to 6
-    const limitedData = data.slice(0, 6)
+    const limitedData = data.slice(0, 6);
 
     // repositories container variable
-    const reposContainer = document.getElementById('reposContainer')
+    const reposContainer = document.getElementById('reposContainer');
 
     // loop through the repos, grab properties
     for (let i = 0; i < limitedData.length; i++) {
-      const repo = limitedData[i]
+      const repo = limitedData[i];
 
-      const repoInfoDiv = document.createElement('div')
+      const repoInfoDiv = document.createElement('div');
       // Add a class
-      repoInfoDiv.classList.add('repo-info')
+      repoInfoDiv.classList.add('repo-info');
 
       repoInfoDiv.innerHTML = `
         <h3>${repo.name}</h3>
         <p class="desc">${repo.description || ''}</p>
-        <p class="language">${repo.language || ''}</p>
+        <ul id="language-${repo.name}"></ul>
         <a href="${repo.html_url}" target="_blank">View on Github &rarr;</a>
-        `
+        `;
 
-      reposContainer.appendChild(repoInfoDiv)
+      reposContainer.appendChild(repoInfoDiv);
+
+      fetch(repo.languages_url)
+        .then((response) => response.json())
+        .then((languagesData) => {
+          const languagesList = document.getElementById(
+            `language-${repo.name}`
+          );
+
+          Object.keys(languagesData).forEach((language) => {
+            const newLanguageEl = document.createElement('li');
+            newLanguageEl.textContent = language;
+            languagesList.appendChild(newLanguageEl);
+          });
+        });
     }
   })
   .catch((error) => {
-    console.error(error)
-  })
+    console.error(error);
+  });
